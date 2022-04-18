6b421fcea79c46e03ad3c712a05201ce318e21de
Žanas Stundys <40335772+zstundys@users.noreply.github.com>
2022-04-18 09:49:03 +0300

    Add ProgressBar styles

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,61 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
-
-const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+const ProgressBar = ({ value, size = "medium" }) => {
+  return (
+    <Root
+      role="progressbar"
+      aria-valuemin={0}
+      aria-valuenow={value}
+      aria-valuemax={100}
+      size={size}
+    >
+      <VisuallyHidden>{value}%</VisuallyHidden>
+      <Bar size={size} style={{ "--width": `${value}%` }} />
+    </Root>
+  );
 };
 
 export default ProgressBar;
+
+const Root = styled.strong`
+  display: block;
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  border-radius: ${({ size }) => borderRadiusBySize[size]};
+  padding: ${({ size }) => paddingBySize[size]};
+  overflow: hidden;
+`;
+
+const Bar = styled.div`
+  background-color: ${COLORS.primary};
+  height: ${({ size }) => barHeightBySize[size]};
+  border-radius: 4px;
+  transition: width 0.2s, border-radius 0.2s;
+  width: var(--width);
+
+  [role="progressbar"]:not([aria-valuenow="100"]) & {
+    border-top-right-radius: 0;
+    border-bottom-right-radius: 0;
+  }
+`;
+
+const paddingBySize = {
+  large: "4px",
+  medium: "0px",
+  small: "0px",
+};
+
+const borderRadiusBySize = {
+  large: "8px",
+  medium: "4px",
+  small: "4px",
+};
+
+const barHeightBySize = {
+  large: "16px",
+  medium: "12px",
+  small: "8px",
+};
